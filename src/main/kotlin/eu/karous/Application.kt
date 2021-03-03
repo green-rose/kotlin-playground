c9472d3588ec4f1f72a24b5a24d1741708bd7855
Mila Karous <m.k.rose@seznam.cz>
2021-03-03 09:45:03 +0100

    Update Application.kt

--- src/main/kotlin/eu/karous/Application.kt
+++ src/main/kotlin/eu/karous/Application.kt
@@ -1,41 +1,42 @@
 package eu.karous
 
+import kotlin.system.measureTimeMillis
+
 enum class PasswordStrength(val description: String, val relativeWeakness: Float) {
    WEAK("weak", 0f),
-   MEDIOCRE("mediocre", 0.25f),
-   STRONG("strong", 0.5f),
-   EXTRA_STRONG("extra strong", 0.75f)
+   MEDIOCRE("mediocre", 0.5f),
+   STRONG("strong", 0.7f),
+   EXTRA_STRONG("extra strong", 0.9f)
 }
 
-fun isSmallLetters(pass: String): Boolean {
+fun containsSmallLetters(pass: String): Boolean {
    return pass.any { it in 'a'..'z' }
 }
-fun isCapitalLetters(pass: String): Boolean {
+fun containsCapitalLetters(pass: String): Boolean {
    return pass.any { it in 'A'..'Z' }
 }
-fun isDigits(pass: String): Boolean {
+fun containsDigits(pass: String): Boolean {
    return pass.any { it in '0'..'9' }
 }
-fun isSpecialCharacters(pass: String): Boolean {
+fun containsSpecialCharacters(pass: String): Boolean {
    return pass.filter { it in 'A'..'Z' || it in 'a'..'z'||  it in '0'..'9' }.length != pass.length
 }
 
 fun passRelativeStrengthChecker(passwordToBeChecked: String): Float {
-   
+
    if (passwordToBeChecked.length < 8) return 0f
 
    var strength: Int = if (passwordToBeChecked.length <= 10) 2 else if (passwordToBeChecked.length <= 14) 4 else 6
 
-   if (isSmallLetters(passwordToBeChecked)) strength += 2
-   if (isCapitalLetters(passwordToBeChecked)) strength += 2
-   if (isDigits(passwordToBeChecked)) strength += 2
-   if (isSpecialCharacters(passwordToBeChecked)) strength += 2
+   if (containsSmallLetters(passwordToBeChecked)) strength += 2
+   if (containsCapitalLetters(passwordToBeChecked)) strength += 2
+   if (containsDigits(passwordToBeChecked)) strength += 2
+   if (containsSpecialCharacters(passwordToBeChecked)) strength += 2
 
-   return (strength - 4).toFloat() / 10
+   return strength.toFloat() / 14
 }
 
 fun main() {
-
 }
 
 fun passChecker(passwordToBeChecked:String):PasswordStrength {
@@ -48,3 +49,5 @@ fun passChecker(passwordToBeChecked:String):PasswordStrength {
 
    return PasswordStrength.WEAK
 }
+
+
